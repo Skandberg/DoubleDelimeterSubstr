776332d6eddbe28499816ddb9b9c4e4595c225ca
Skandberg <ilarion.larin@gmail.com>
2020-10-14 19:08:46 +0300

    Update 13.10.2020

--- MaxString.py
+++ MaxString.py
@@ -1,11 +1,8 @@
 def maxStringWithoutRepeat(word):
-    temp_word1= word.lower()
-    temp_word2=[]
-    for i in temp_word1:
-        if i not in temp_word2:
-            temp_word2.append(i)
-    temp_word1 = ''.join(temp_word2)
-    del temp_word2
-    return (len(temp_word1))
+    temp_word=[]
+    for i in list(word.lower().strip()):
+        if i not in temp_word:
+            temp_word.append(i)    
+    return (len(temp_word))
     
 
--- test.py
+++
@@ -1,9 +0,0 @@
-test_word = 'Bob Marley'
-test_word=test_word.lower()
-test_word2 = []
-for i in test_word:
-    if i not in test_word2:
-        test_word2.append(i)
-test_word1 = ''.join(test_word2)
-del test_word2
-print(len(test_word1))
